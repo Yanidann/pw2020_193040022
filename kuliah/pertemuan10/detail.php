3aa86dec84ba83333b817946a7ab6eca0904c1d8
YanidaNurNabila <60532297+YanidaNurNabila@users.noreply.github.com>
2020-04-20 13:00:39 +0700

    Tugas Pertemuan 10

---
+++ kuliah/pertemuan10/detail.php
@@ -0,0 +1,40 @@
+<?php
+require 'functions.php';
+
+// ambil id dari URL
+$id = $_GET['id'];
+
+// query mahasiswa berdasarkan id
+$m = query("SELECT * FROM mahasiswa WHERE id = $id")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Mahasiswa</title>
+
+  <style>
+    img {
+      width: 100px;
+      height: 100px;
+    }
+  </style>
+</head>
+
+<body>
+  <h3>Detail Mahasiswa</h3>
+  <ul>
+    <li><img src="img/<?= $m['gambar']; ?>"></li>
+    <li>NRP: <?= $m['nrp']; ?></li>
+    <li>Nama: <?= $m['nama']; ?></li>
+    <li>Email: <?= $m['email']; ?></li>
+    <li>Jurusan: <?= $m['jurusan']; ?></li>
+    <li><a href="">ubah</a> | <a href="">hapus</a></li>
+    <li><a href="latihan3.php">kembali kedaftar mahasiswa</a></li>
+  </ul>
+</body>
+
+</html>
