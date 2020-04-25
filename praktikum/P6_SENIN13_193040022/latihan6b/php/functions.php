25dc7a5a7839e00344ebd016336b6cf95d12dbd1
YanidaNurNabila <60532297+YanidaNurNabila@users.noreply.github.com>
2020-04-25 16:46:17 +0700

    PRAKTIKUM MODUL 6

---
+++ praktikum/P6_SENIN13_193040022/latihan6b/php/functions.php
@@ -0,0 +1,41 @@
+<?php
+// function untuk melakukan koneksi ke database
+function koneksi()
+{
+    $conn = mysqli_connect("localhost", "root", "") or die("Koneksi ke DB gagal!");
+    mysqli_select_db($conn, "tubes_193040022") or die("Database Salah!");
+
+    return $conn;
+}
+
+// function untuk melakukan query ke database
+function query($sql)
+{
+    $conn = koneksi();
+    $result = mysqli_query($conn, "$sql");
+
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+// fungsi untuk menambahkan data didalam database
+function tambah($data)
+{
+    $conn = koneksi();
+
+    $gambar = htmlspecialchars($data['gambar']);
+    $nama = htmlspecialchars($data['nama']);
+    $asal = htmlspecialchars($data['asal']);
+    $bahan = htmlspecialchars($data['bahan']);
+    $harga = htmlspecialchars($data['harga']);
+
+    $query = "INSERT INTO makanan
+                    VALUES
+                    ('', '$nama', '$asal', '$bahan', '$harga', '$gambar')";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
