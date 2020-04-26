b6b79d09ccec9151bfcab9f34f23a2b94c81daff
YanidaNurNabila <60532297+YanidaNurNabila@users.noreply.github.com>
2020-04-27 05:51:40 +0700

    PRAKTIKUM MODUL 6

---
+++ praktikum/P6_SENIN13_193040022/latihan6e/php/ubah.php
@@ -0,0 +1,64 @@
+<?php
+require 'functions.php';
+
+$id = $_GET['id'];
+$m = query("SELECT * FROM makanan WHERE id = $id")[0];
+
+if (isset($_POST['ubah'])) {
+  if (ubah($_POST) > 0) {
+    echo "<script>
+              alert('Data Berhasil diubah!');
+              document.location.href = 'admin.php';
+          </script>";
+  } else {
+    echo "<script>
+              alert('Data Gagal diubah!');
+              document.location.href = 'admin.php';
+          </script>";
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>halaman ubah form</title>
+</head>
+
+<body>
+  <h3>Form Ubah Daftar Makanan</h3>
+  <form action="" method="post">
+    <input type="hidden" name="id" id="id" value="<?= $m['id']; ?>">
+    <ul>
+      <li>
+        <label for="gambar">Gambar:</label><br>
+        <input type="text" name="gambar" id="gambar" required value="<?= $m['gambar']; ?>"><br><br>
+      </li>
+      <li>
+        <label for="nama">Nama Makanan:</label><br>
+        <input type="text" name="nama" id="nama" required value="<?= $m['nama']; ?>"><br><br>
+      </li>
+      <li>
+        <label for="asal">Asal Makanan:</label><br>
+        <input type="text" name="asal" id="asal" required value="<?= $m['asal']; ?>"><br><br>
+      </li>
+      <li>
+        <label for="bahan">Bahan Makanan:</label><br>
+        <input type="text" name="bahan" id="bahan" required value="<?= $m['bahan']; ?>"><br><br>
+      </li>
+      <li>
+        <label for="harga">Harga:</label><br>
+        <input type="text" name="harga" id="harga" required value="<?= $m['harga']; ?>"><br><br>
+      </li>
+      <button type="submit" name="ubah">Ubah Data!</button>
+      <button type="submit">
+        <a href="../index.php" style="text-decoration: none; color: black;">Kembali</a>
+      </button>
+    </ul>
+  </form>
+</body>
+
+</html>
