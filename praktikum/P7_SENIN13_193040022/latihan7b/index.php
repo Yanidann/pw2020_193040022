32bea179148052fec069f55572980a25e767c525
YanidaNurNabila <60532297+YanidaNurNabila@users.noreply.github.com>
2020-05-03 05:13:19 +0700

    PRAKTIKUM MODUL 7

---
+++ praktikum/P7_SENIN13_193040022/latihan7b/index.php
@@ -0,0 +1,95 @@
+<?php
+// menghubungkan dengan file php lainnya
+require 'php/functions.php';
+
+// melakukan query
+$makanan = query("SELECT * FROM makanan");
+
+if (isset($_POST['cari'])) {
+    $makanan = cari($_POST['keyword']);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan 7a</title>
+
+    <style>
+        .container {
+            border: 4px solid #191970;
+            /* midnight blue */
+            background-color: #48D1CC;
+            /* medium turquoise */
+            width: 300px;
+            height: 300px;
+            margin: auto;
+            padding: 30px 0 20px 20px;
+            font-family: arial;
+            font-size: x-large;
+        }
+
+        a:link {
+            text-decoration: none;
+            color: #0000CD;
+            /* medium blue */
+        }
+
+        a:hover {
+            text-decoration: none;
+            color: #FFFF00;
+            /* yellow */
+        }
+
+        a:active {
+            text-decoration: none;
+        }
+    </style>
+
+</head>
+
+<body>
+    <h1>
+        <center>DAFTAR MAKANAN</center>
+    </h1>
+
+    <form action="" method="POST">
+        <center><input type="text" name="keyword" size="25" placeholder="masukan pencarian.." autocomplete="off" autofocus>
+            <button type="submit" name="cari">Cari!</button></center>
+    </form>
+    <br>
+
+    <center><a href="php/login.php">
+            <button>Masuk ke halaman admin</button>
+        </a></center>
+    <br>
+
+    <div class="container">
+
+        <?php if (empty($makanan)) : ?>
+            <tr>
+                <td colspan="4">
+                    <h1 style="color: red; font-style: italic; font-size: large;">Data tidak ditemukan</h1>
+                </td>
+            </tr>
+        <?php endif; ?>
+
+
+        <?php foreach ($makanan as $mkn) : ?>
+            <div class="nama">
+                <a href="php/detail.php?id=<?= $mkn['id']; ?>">
+                    <?= $mkn["nama"]; ?>
+                </a>
+            </div>
+        <?php endforeach; ?>
+    </div>
+    <h1>
+        <center>TERIMAKASIH SUDAH MEMESAN!</center>
+    </h1>
+
+</body>
+
+</html>
---
+++ praktikum/P7_SENIN13_193040022/latihan7b/php/functions.php
@@ -0,0 +1,123 @@
+<?php
+// function untuk melakukan koneksi ke database
+function koneksi()
+{
+    $conn = mysqli_connect("localhost", "root", "") or die("Koneksi ke DB gagal!");
+    mysqli_select_db($conn, "tubes_193040022") or die("Database Salah!");
+
+    return $conn;
+}
+
+// function untuk melakukan query ke database
+function query($sql)
+{
+    $conn = koneksi();
+    $result = mysqli_query($conn, "$sql");
+
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+// fungsi untuk menambahkan data didalam database
+function tambah($data)
+{
+    $conn = koneksi();
+
+    $gambar = htmlspecialchars($data['gambar']);
+    $nama = htmlspecialchars($data['nama']);
+    $asal = htmlspecialchars($data['asal']);
+    $bahan = htmlspecialchars($data['bahan']);
+    $harga = htmlspecialchars($data['harga']);
+
+    $query = "INSERT INTO makanan
+                    VALUES
+                    ('', '$nama', '$asal', '$bahan', '$harga', '$gambar')";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+// fungsi untuk menghapus data
+function hapus($id)
+{
+    $conn = koneksi();
+    mysqli_query($conn, "DELETE FROM makanan WHERE id = $id");
+
+    return mysqli_affected_rows($conn);
+}
+
+// fungsi untuk mengubah data
+function ubah($data)
+{
+    $conn = koneksi();
+
+    $id = htmlspecialchars($data['id']);
+    $gambar = htmlspecialchars($data['gambar']);
+    $nama = htmlspecialchars($data['nama']);
+    $asal = htmlspecialchars($data['asal']);
+    $bahan = htmlspecialchars($data['bahan']);
+    $harga = htmlspecialchars($data['harga']);
+
+    $query = "UPDATE makanan
+              SET
+                nama = '$nama', 
+                asal = '$asal', 
+                bahan = '$bahan', 
+                harga = '$harga', 
+                gambar = '$gambar'
+                WHERE id = '$id'
+                ";
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+// cari
+function cari($keyword)
+{
+    $conn = koneksi();
+
+    $query = "SELECT * FROM makanan
+              WHERE 
+              nama LIKE '%$keyword%' OR 
+              asal LIKE '%$keyword%' OR
+              bahan LIKE '%$keyword%' OR
+              harga LIKE '%$keyword%'
+              ";
+    $result = mysqli_query($conn, $query);
+
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+// registrasi
+function registrasi($data)
+{
+    $conn = koneksi();
+    $username = strtolower(stripcslashes($data['username']));
+    $password = mysqli_real_escape_string($conn, $data['password']);
+
+    // cek username sudah ada atau belum
+    $result = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username' ");
+    if (mysqli_fetch_assoc($result)) {
+        echo "<script>
+                alert('username sudah digunakan');
+              </script>";
+    }
+
+    // enkripsi password
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    // tambah user baru
+    $query_tambah = "INSERT INTO user VALUES('', '$username', '$password')";
+    mysqli_query($conn, $query_tambah);
+
+    return mysqli_affected_rows($conn);
+}
