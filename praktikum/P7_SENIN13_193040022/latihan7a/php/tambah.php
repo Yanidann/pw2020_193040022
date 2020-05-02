70057f9fc80efb9f1b1d9d51cbc57c3142e5af39
YanidaNurNabila <60532297+YanidaNurNabila@users.noreply.github.com>
2020-05-03 03:32:08 +0700

    PRAKTIKUM MODUL 7

---
+++ praktikum/P7_SENIN13_193040022/latihan7a/php/tambah.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login'])) {
+  header("Location: login.php");
+  exit;
+}
+
+require 'functions.php';
+
+if (isset($_POST['tambah'])) {
+  if (tambah($_POST) > 0) {
+    echo "<script>
+              alert('Data Berhasil ditambahkan!');
+              document.location.href = 'admin.php';
+          </script>";
+  } else {
+    echo "<script>
+              alert('Data Gagal ditambahkan!');
+              document.location.href = 'admin.php';
+          </script>";
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>halaman tambah form</title>
+</head>
+
+<body>
+  <h3>Form Tambah Daftar Makanan</h3>
+  <form action="" method="post">
+    <ul>
+      <li>
+        <label for="gambar">Gambar:</label><br>
+        <input type="text" name="gambar" id="gambar" required><br><br>
+      </li>
+      <li>
+        <label for="nama">Nama Makanan:</label><br>
+        <input type="text" name="nama" id="nama" required><br><br>
+      </li>
+      <li>
+        <label for="asal">Asal Makanan:</label><br>
+        <input type="text" name="asal" id="asal" required><br><br>
+      </li>
+      <li>
+        <label for="bahan">Bahan Makanan:</label><br>
+        <input type="text" name="bahan" id="bahan" required><br><br>
+      </li>
+      <li>
+        <label for="harga">Harga:</label><br>
+        <input type="text" name="harga" id="harga" required><br><br>
+      </li>
+      <button type="submit" name="tambah">Tambah Data!</button>
+      <button type="submit">
+        <a href="index.php" style="text-decoration: none; color: black;">Kembali</a>
+      </button>
+    </ul>
+  </form>
+</body>
+
+</html>
