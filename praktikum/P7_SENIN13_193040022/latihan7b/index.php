c4810e91939833936726dc325c37f2f107fc1c64
YanidaNurNabila <60532297+YanidaNurNabila@users.noreply.github.com>
2020-05-07 00:08:16 +0700

    PRAKTIKUM MODUL 7

--- praktikum/P7_SENIN13_193040022/latihan7b/index.php
+++ praktikum/P7_SENIN13_193040022/latihan7b/index.php
@@ -16,7 +16,7 @@ if (isset($_POST['cari'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>latihan 7a</title>
+    <title>latihan 7b</title>
 
     <style>
         .container {
