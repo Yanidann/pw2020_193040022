ef93abd70bf6bcd79e216df34ac344cd7c126a73
YanidaNurNabila <60532297+YanidaNurNabila@users.noreply.github.com>
2020-05-13 15:14:13 +0700

    tubes

--- tubes/php/tambah.php
+++ tubes/php/tambah.php
@@ -67,11 +67,11 @@ if (isset($_POST['tambah'])) {
           <hr>
 
           <!-- start form login -->
-          <form action="" method="post">
+          <form action="" method="POST" enctype="multipart/form-data">
             <div class="form-group">
               <div class="form-group">
                 <label for="gambar">Gambar :</label>
-                <input type="text" class="form-control" name="gambar" id="gambar" required>
+                <input type="file" class="form-control" name="gambar" id="gambar">
               </div>
             </div>
             <div class="form-group">
